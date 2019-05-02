8d7857bf02c6af9d89557d46f4a81160ee10a433
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-05-02 15:36:51 +0100

    When the customer pays the exact amount for the item, the vending machine dispenses the item and does not dispense any change.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,1 +1,9 @@
-VendingMachine = Class.new
+class VendingMachine
+  def initialize(item_dispenser:, change_dispenser:)
+    @item_dispenser = item_dispenser
+  end
+
+  def dispense(product, amount_payed)
+    @item_dispenser.dispense(product)
+  end
+end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,13 +1,31 @@
 require './lib/vending_machine'
 describe VendingMachine do
+  let(:item_dispenser) { double(:item_dispenser).as_null_object }
+  let(:change_dispenser) { double(:change_dispenser).as_null_object }
+
+  subject(:vending_machine) do
+    VendingMachine.new(
+      item_dispenser: item_dispenser,
+      change_dispenser: change_dispenser,
+    )
+  end
+
   describe '#purchase' do
+    let(:product_number) { '25' }
+    let(:amount_payed) { 50 }
+
     context 'when the customer pays the correct amount for the item' do
-      it 'dispenses the item'
+      it 'dispenses the item' do
+        expect(item_dispenser).to receive(:dispense).with(product_number)
 
-      it 'dispenses no change'
+        vending_machine.dispense(product_number, amount_payed)
+      end
 
-      it 'asks the customer to collect their item'
+      it 'dispenses no change' do
+        expect(change_dispenser).not_to receive(:dispense)
+
+        vending_machine.dispense(product_number, amount_payed)
+      end
     end
-
   end
 end
