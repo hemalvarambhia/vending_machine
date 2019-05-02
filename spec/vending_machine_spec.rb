918e2d95984e2bfd5f39cc15ca2e847e36ba6cd9
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-05-02 14:50:37 +0100

    Initial test list - when the customer pays the correct amount for their item.

---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,13 @@
+require './lib/vending_machine'
+describe VendingMachine do
+  describe '#purchase' do
+    context 'when the customer pays the correct amount for the item' do
+      it 'dispenses the item'
+
+      it 'dispenses no change'
+
+      it 'asks the customer to collect their item'
+    end
+
+  end
+end
