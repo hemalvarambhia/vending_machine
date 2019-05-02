55703a5d77f61476897e57213d783827bbd4e76f
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-05-02 18:02:59 +0100

    Perhaps the machine should return the customer their money back.

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -87,9 +87,9 @@ describe VendingMachine do
     end
 
     context 'when the product is not listed in the catalogue' do
-      it 'asks the user to select a different product'
+      it 'returns the customer their money back'
 
-      it 'does not dispense any change'
+      it 'asks the user to select a different product'
 
       it 'does not dispense any product'
     end
