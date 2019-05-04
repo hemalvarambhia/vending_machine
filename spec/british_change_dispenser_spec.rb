0011bc957606c7a75be32adc3e3f3d5a95ce696f
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-05-04 21:02:23 +0100

    Added interesting test cases.

--- spec/british_change_dispenser_spec.rb
+++ spec/british_change_dispenser_spec.rb
@@ -55,5 +55,69 @@ describe BritishChangeDispenser do
         end
       end
     end
+
+    context '£1.30' do
+      context 'given we have £1, 20p and 10p coins available' do
+        subject(:change_dispenser) do
+          described_class.new(100 => 2, 20 => 10, 10 => 4)
+        end
+
+        let(:expected) { [100, 20, 10] }
+        
+        it 'returns the correct change' do
+          expect(change_dispenser.change_for(130)).to eq expected
+        end
+      end
+    end
+
+    context '£3.88' do
+      context 'given we have the necessary coins available' do
+        subject(:change_dispenser) do
+          described_class.new(
+            200 => 1000,
+            100 => 2,
+            50 => 50,
+            20 => 10,
+            10 => 4,
+            5 => 10,
+            2 => 75,
+            1 => 1000
+          )
+        end
+
+        let(:expected) do
+          [200, 100, 50, 20, 10, 5, 2, 1]
+        end
+
+        it 'returns the correct change' do
+          expect(change_dispenser.change_for(388)).to eq expected
+        end
+      end
+    end
+
+    context '£5.85' do
+      context 'given we have the necessary coins available' do
+        subject(:change_dispenser) do
+          described_class.new(
+            200 => 1000,
+            100 => 2,
+            50 => 50,
+            20 => 10,
+            10 => 4,
+            5 => 10,
+            2 => 75,
+            1 => 1000
+          )
+        end
+
+        let(:expected) do
+          [200, 200, 100, 50, 20, 10, 5]
+        end
+
+        it 'returns the correct change' do
+          expect(change_dispenser.change_for(585)).to eq expected
+        end
+      end
+    end
   end
 end
